65eda72dc4e356aecd3ac132144d4254b30613ad
anuchard.a <anuchard.a@192.168.1.2>
2017-03-26 22:42:38 +0700

    Write log

--- src/th/co/ais/cpac/cl/batch/util/GenFileUtil.java
+++ src/th/co/ais/cpac/cl/batch/util/GenFileUtil.java
@@ -1,12 +1,37 @@
 package th.co.ais.cpac.cl.batch.util;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class GenFileUtil {
-	public static void genFile(String[] genData,String fileName,String outBoundPath){
+	
+	public static final String lineSeparator = System.getProperty("line.separator");
+	
+	public static void genFile(String[] genData,String fileName,String outBoundPath,String encode) throws Exception{
+		String fileNamePath = outBoundPath+"/"+fileName;
+		 Writer writer = null;
+		 try{
+			 writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileNamePath), encode));
+			 writer.write("Header");
+			 for(int i=0; i<genData.length; i++){
+				 writer.write(genData[i]);
+				 writer.write(lineSeparator);
+			 }
+			 writer.write("Footer");
+		 }catch(Exception e){
+			 throw e;
+		 }finally{
+			 try {writer.close();} catch (Exception ex) {/*ignore*/}
+		 }
+		 
 		//1.Gen Data (fileName)
 			//01|20070828_160817
 			//genData ->body
@@ -23,6 +48,19 @@ public class GenFileUtil {
 		String yyyymmddStr = format.format(cal.getTime());
 		return nameFormat.replaceAll("yyyymmdd_hh24miss", yyyymmddStr);
 		
+	}
+	
+	public static void createFile(String fileName, String filePath) throws IOException{
+		String fileNamePath = filePath+"/"+fileName;
+		createFile(fileNamePath);
+	}
+	
+	public static void createFile(String fileNamePath) throws IOException{
+        File file = new File(fileNamePath);
+        // If file doesn't exists, then create it
+        if (!file.exists()) {
+            file.createNewFile();
+        }
 	}
 	
 }
